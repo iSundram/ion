aa10a5f4f082891ac9f9ca9c24e56cc41567c74e
Sundram Kumar Tiwari <abhayshankart1@gmail.com>
2025-09-14 03:43:07 +0000

    Checkpoint from VS Code for coding agent session

---
+++ AdminiTheme control panel (1.0.1)/PHP 8.1/modules/addons/adminiTheme/classes/templates.class.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * AdminiTheme - WHMCS Theme Module
+ * Professional WHMCS Administration Theme
+ */
+$supportedTemplates = [];
+$supportedTemplates["emyui"] = ["short" => "emyui", "name" => "AdminiTheme WHMCS theme", "file" => "emyui.tpl"];
+
+?>
